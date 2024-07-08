84d670e58da67e2875bc11c369e513665f814b36
Leeseoleem <leeseorim0029@gmail.com>
2024-07-08 20:56:31 +0900

    react-beginner-create-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyWord, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = function () {
+    setValue(counter + 1);
+  };
+  console.log("counter is update"); //state가 변경될 때마다 실행
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+  useEffect(() => {
+    if (keyWord !== "" && keyWord.length > 5) {
+      console.log("SERCH FOR: ", keyWord);
+    }
+  }, [keyWord]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="serch here..."
+        value={keyWord}
+        onChange={onChange}
+      />
+      <h1 className={styles.title}>클릭 한 횟수: {counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
